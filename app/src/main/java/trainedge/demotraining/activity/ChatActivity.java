f9bcfe94f0b76c5af3654c68b9d7c95879f6082b
Mridul Tripathi <mridultripathi1408@gmail.com>
2017-09-25 13:39:23 +0530

    not fully complete

--- app/src/main/java/trainedge/demotraining/activity/ChatActivity.java
+++ app/src/main/java/trainedge/demotraining/activity/ChatActivity.java
@@ -1,30 +1,10 @@
 package trainedge.demotraining.activity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.provider.SyncStateContract;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import trainedge.demotraining.R;
-import trainedge.demotraining.model.User;
-import trainedge.demotraining.utils.SharedPrefUtil;
-import trainedge.demotraining.utils.Constants;
 
 public class ChatActivity extends AppCompatActivity{
 
