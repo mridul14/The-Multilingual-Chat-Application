5ab186207d2b09dfaff9fa5cda6482ff72aa8dbc
Mridul Tripathi <mridultripathi1408@gmail.com>
2017-09-29 12:13:12 +0530

    not fully complete

--- app/src/main/java/trainedge/demotraining/activity/NextActivity.java
+++ app/src/main/java/trainedge/demotraining/activity/NextActivity.java
@@ -1,13 +1,24 @@
 package trainedge.demotraining.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
+import devlight.io.library.ntb.NavigationTabBar;
 import trainedge.demotraining.R;
+import trainedge.demotraining.fragment.AddContactFragment;
+import trainedge.demotraining.fragment.ChatFragment;
+import trainedge.demotraining.fragment.InviteFragment;
 
 public class NextActivity extends AppCompatActivity {
 
@@ -41,10 +52,60 @@ public class NextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
+        viewPager.setAdapter(new MyAdapter(getSupportFragmentManager()));
+        final String[] colors = getResources().getStringArray(R.array.default_preview);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_add_black_24dp),
+                        Color.parseColor(colors[0]))
+                        .title("Add Contact")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_contacts_black_24dp),
+                        Color.parseColor(colors[1]))
+                        .title("Chat")
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_share_black_24dp),
+                        Color.parseColor(colors[2]))
+                        .title("Invite")
+                        .build()
+        );
+        navigationTabBar.setModels(models);
+        navigationTabBar.setViewPager(viewPager, 1);
     }
 
+    public class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return AddContactFragment.newInstance("","");
+                case 1:
+                    return ChatFragment.newInstance("","");
+                case 2:
+                    return InviteFragment.newInstance("","");
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
 
 
 }
--- app/src/main/java/trainedge/demotraining/fragment/AddContactFragment.java
+++ app/src/main/java/trainedge/demotraining/fragment/AddContactFragment.java
@@ -13,7 +13,6 @@ import trainedge.demotraining.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AddContactFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link AddContactFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -28,7 +27,6 @@ public class AddContactFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
     public AddContactFragment() {
         // Required empty public constructor
@@ -68,42 +66,5 @@ public class AddContactFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_add_contact, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
