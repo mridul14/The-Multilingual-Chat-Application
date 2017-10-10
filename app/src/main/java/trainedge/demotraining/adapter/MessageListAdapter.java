be0c388be7002c01aa1959914708be12d0c177e7
Mridul Tripathi <mridultripathi1408@gmail.com>
2017-10-10 19:41:53 +0530

    not fully complete

---
+++ app/src/main/java/trainedge/demotraining/adapter/MessageListAdapter.java
@@ -0,0 +1,8 @@
+package trainedge.demotraining.adapter;
+
+/**
+ * Created by dell on 10-10-2017.
+ */
+
+public class MessageListAdapter {
+}
---
+++ app/src/main/java/trainedge/demotraining/holder/ReceiveMessageHolder.java
@@ -0,0 +1,8 @@
+package trainedge.demotraining.holder;
+
+/**
+ * Created by dell on 10-10-2017.
+ */
+
+public class ReceiveMessageHolder {
+}
